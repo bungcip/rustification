4e70a804f9f60cf0324fd4d1612a9900ed43c616
bungcip <bungcip@gmail.com>
2025-05-28 21:50:00 +0700

    patch from c2rust "c2rust-transpile: Use a separate file for the generated rust-toolchain.toml"
    with some modification

--- c2rust-transpile/src/build_files/mod.rs
+++ c2rust-transpile/src/build_files/mod.rs
@@ -260,12 +260,8 @@ fn emit_lib_rs(
 /// If we translate variadic functions, the output will only compile
 /// on a nightly toolchain until the `c_variadics` feature is stable.
 fn emit_rust_toolchain(tcfg: &TranspilerConfig, build_dir: &Path) {
-    let output = r#"
-    [toolchain]
-    channel = "nightly"
-    "#;
-
     let output_path = build_dir.join("rust-toolchain.toml");
+    let output = include_str!("generated-rust-toolchain.toml").to_string();
     maybe_write_to_file(&output_path, output.to_string(), tcfg.overwrite_existing);
 }
 
